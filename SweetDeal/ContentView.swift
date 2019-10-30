67a25366d4ef6c1254de357a6c56a80cc417c748
Sandy Pan <sqp@andrew.cmu.edu>
2019-10-30 17:57:00 -0400

    Initial Commit

---
+++ SweetDeal/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SweetDeal
+//
+//  Created by Sandy Pan on 10/30/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
