f8c2ede4e600b48bf004ad3f5cc30705b55e6a82
Bharath Prabhu <bharathp@bh140d-a3.andrew.cmu.edu>
2019-11-03 17:23:46 -0500

    Base Code for Project

---
+++ SweetDeal/SweetDeal/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SweetDeal
+//
+//  Created by Bharath Prabhu on 11/3/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
