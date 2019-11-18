f9a46b0e882523b0d73a3fe4267cf18bc2aedb62
Sandy Pan <sqp@andrew.cmu.edu>
2019-11-17 22:15:21 -0500

    Firebase configuration and Google Sign In

--- SweetDeal/SweetDeal/AppDelegate.swift
+++ SweetDeal/SweetDeal/AppDelegate.swift
@@ -7,17 +7,57 @@
 
 import UIKit
 import CoreData
+import Firebase
+import GoogleSignIn
+
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        GIDSignIn.sharedInstance().clientID = "318021857036-791l2cpodesfjkhlafejqq632vbrgol9.apps.googleusercontent.com"
+        GIDSignIn.sharedInstance().delegate = self
         return true
     }
+  
+    @available(iOS 9.0, *)
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+      return GIDSignIn.sharedInstance().handle(url)
+    }
+  
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!,
+              withError error: Error!) {
+      if let error = error {
+        if (error as NSError).code == GIDSignInErrorCode.hasNoAuthInKeychain.rawValue {
+          print("The user has not signed in before or they have since signed out.")
+        } else {
+          print("\(error.localizedDescription)")
+        }
+        return
+      }
+      // Perform any operations on signed in user here.
+//      let userId = user.userID                  // For client-side use only!
+//      let idToken = user.authentication.idToken // Safe to send to the server
+      let fullName = user.profile.name
+      print(fullName)
+//      let givenName = user.profile.givenName
+//      let familyName = user.profile.familyName
+//      let email = user.profile.email
+//      guard let authentication = user.authentication else { return }
+//      let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
+//                                                     accessToken: authentication.accessToken)
+    }
+  
+    func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!,
+              withError error: Error!) {
+      // Perform any operations when the user disconnects from app here.
+    }
+  
+  
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- SweetDeal/SweetDeal/ViewControllers/ViewController.swift
+++ SweetDeal/SweetDeal/ViewControllers/ViewController.swift
@@ -2,13 +2,23 @@ import UIKit
 import CoreLocation
 import UserNotifications
 import MapKit
+import GoogleSignIn
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
+  
     
-    let locationManager = CLLocationManager()
+  @IBAction func googleSignInClicked(_ sender: GIDSignInButton) {
+//    sender.setTitle("Sign in with Google", for: .normal)
+     GIDSignIn.sharedInstance().signIn()
+  }
+  let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+      
+       //  Google Sign in
+        GIDSignIn.sharedInstance()?.presentingViewController = self
+        GIDSignIn.sharedInstance()?.restorePreviousSignIn()
         
         // App need to run in background
         locationManager.requestAlwaysAuthorization()
