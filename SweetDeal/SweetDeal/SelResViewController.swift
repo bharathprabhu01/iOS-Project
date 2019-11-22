2bd1b1f5f57cf578468b518ae2dc16102bf239e2
Sandy Pan <sqp@andrew.cmu.edu>
2019-11-21 23:23:17 -0500

    read Yelp data from Firebase and stored it into a dictionary

--- SweetDeal/SweetDeal/SelResViewController.swift
+++ SweetDeal/SweetDeal/SelResViewController.swift
@@ -6,19 +6,40 @@
 //
 
 import UIKit
+import Firebase
 
-class SelResViewController: UIViewController {
-  var currUserID:String = ""
-  var currUserFN:String = ""
-  var currUserLN:String = ""
-  var currUserEmail:String = ""
+class SelResViewController: UIViewController{
+  var ref: DatabaseReference!
+  var currUserID: String = ""
+  var currUserFN: String = ""
+  var currUserLN: String = ""
+  var currUserEmail: String = ""
+  var restaurantList: Dictionary<String, AnyObject> = [:]
   
   @IBOutlet weak var currUserLabel: UILabel!
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    ref = Database.database().reference()
     currUserLabel.text = "Welcome " + currUserFN + ","
+    ref.child("Restaurants").observeSingleEvent(of: .value, with: { (snapshot) in
+      if let resList = snapshot.value as? [String: AnyObject] {
+        self.restaurantList = resList
+//        print(self.restaurantList)
+//        self.idLabel.text = dictionary["postID"] as? String
+      }
+    })
+    
   }
+  
+//  func readDeal(
+//    return ref.("/users/" + self.currUserID).once("value").then(function(snapshot) {
+//
+//    var username = (snapshot.val() && snapshot.val().username) || "Anonymous";
+//
+//    });)
+  
+  
   
   
     
--- SweetDeal/SweetDeal/StuResViewController.swift
+++ SweetDeal/SweetDeal/StuResViewController.swift
@@ -6,13 +6,8 @@
 //
 
 import UIKit
-import Firebase
 
 class StuResViewController: UIViewController {
-//  let delegate = UIApplication.shared.delegate as! AppDelegate
-//  let delegate = UIApplication.shared.delegate as! AppDelegate
-//  let delegate = GIDSignIn.sharedInstance().delegate as! AppDelegate
-  var ref: DatabaseReference!
   var currUserID: String?
   var currUserFN: String?
   var currUserLN: String?
@@ -20,7 +15,6 @@ class StuResViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    ref = Database.database().reference()
   }
   
   override func prepare(for segue: UIStoryboardSegue, sender: Any?)
@@ -30,21 +24,7 @@ class StuResViewController: UIViewController {
       vc?.currUserID = self.currUserID!
       vc?.currUserFN = self.currUserFN!
       vc?.currUserLN = self.currUserLN!
-      vc?.currUserEmail = self.currUserEmail!
+      vc?.currUserEmail = currUserEmail!
     }
   }
-
-//
-//
-//    /*
-//    // MARK: - Navigation
-//
-//    // In a storyboard-based application, you will often want to do a little preparation before navigation
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        // Get the new view controller using segue.destination.
-//        // Pass the selected object to the new view controller.
-//    }
-//    */
-//
-//}
 }
