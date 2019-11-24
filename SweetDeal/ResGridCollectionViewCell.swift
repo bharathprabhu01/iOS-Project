4bc6f3878277e8bfe28c01a791ddb94caebebc99
Sandy Pan <sqp@andrew.cmu.edu>
2019-11-23 19:34:37 -0500

    collection view showing restaurant list

---
+++ SweetDeal/ResGridCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ResGridCollectionViewCell.swift
+//  
+//
+//  Created by Sandy Pan on 11/23/19.
+//
+
+import UIKit
+
+class ResGridCollectionViewCell: UICollectionViewCell {
+    
+  @IBOutlet weak var resNameLabel: UILabel!
+  @IBOutlet weak var resPic: UIImageView!
+}
--- SweetDeal/SweetDeal/SelResViewController.swift
+++ SweetDeal/SweetDeal/SelResViewController.swift
@@ -1,57 +1,80 @@
-//
-//  SelResViewController.swift
-//  SweetDeal
-//
-//  Created by Sandy Pan on 11/21/19.
-//
-
 import UIKit
 import Firebase
 
-class SelResViewController: UIViewController{
+class SelResViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
   var ref: DatabaseReference!
   var currUserID: String = ""
   var currUserFN: String = ""
   var currUserLN: String = ""
   var currUserEmail: String = ""
+  var cellColor = true
   var restaurantList: Dictionary<String, AnyObject> = [:]
+  var restaurants = [Restaurant]()
   
   @IBOutlet weak var currUserLabel: UILabel!
+  @IBOutlet weak var resGrid: UICollectionView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    resGrid.delegate = self
+    resGrid.dataSource = self
     ref = Database.database().reference()
     currUserLabel.text = "Welcome " + currUserFN + ","
-    ref.child("Restaurants").observeSingleEvent(of: .value, with: { (snapshot) in
-      if let resList = snapshot.value as? [String: AnyObject] {
-        self.restaurantList = resList
-//        print(self.restaurantList)
-//        self.idLabel.text = dictionary["postID"] as? String
-      }
-    })
-    
+    retrieveRestaurants()
+  }
+      
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    print(self.restaurants.count)
+    return self.restaurants.count
   }
   
-//  func readDeal(
-//    return ref.("/users/" + self.currUserID).once("value").then(function(snapshot) {
-//
-//    var username = (snapshot.val() && snapshot.val().username) || "Anonymous";
-//
-//    });)
-  
-  
-  
-  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "restaurantCell", for: indexPath) as! ResGridCollectionViewCell
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+//    print(self.restaurantList[indexPath.row])
+//    var restaurant = ""
+//    restaurant = self.restaurantList[indexPath.row]
+    print(self.restaurants[indexPath.row].name)
+//    cell.resPic.image = self.restaurants[indexPath.row].imageURL
+    cell.resNameLabel.text = self.restaurants[indexPath.row].name
+    return cell
+  }
+  
+  func retrieveRestaurants() {
+    var name: String = ""
+    var imageURL: String = ""
+    var phone: String = ""
+    ref.child("Restaurants").observeSingleEvent(of: .value, with: { (snapshot) in
+      if let resList = snapshot.value as? [String: AnyObject] {
+        for obj in resList.values {
+          if let tempName = obj["name"] {
+            if let resName = tempName as? String {
+              name = resName
+            }
+          }
+          if let tempPhone = obj["phone"] {
+            if let resPhone = tempPhone as? String {
+              phone = resPhone
+            }
+          }
+        if let tempImageURL = obj["imageURL"] {
+          if let resImageURL = tempImageURL as? String {
+            imageURL = resImageURL
+          }
+        }
+        var res = Restaurant(name: name, phone: phone, imageURL: imageURL)
+        self.restaurants.append(res)
+        DispatchQueue.main.async {
+          self.resGrid.reloadData()
+        }
+      }
+      }
+    })
+  }
 }
+
